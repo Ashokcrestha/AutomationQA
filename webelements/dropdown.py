c0dcfb53229689382b96cd248d2b20faba558fb5
Ashokcrestha <ashokstha51@gmail.com>
2025-11-24 18:37:22 +0545

    Add

--- webelements/dropdown.py
+++ webelements/dropdown.py
@@ -5,3 +5,8 @@ from selenium.webdriver.support.ui import Select
 
 driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/dropdown")
+
+dropdown=Select(driver.find_element(By.ID,"dropdown"))
+dropdown.select_by_visible_text("Option 1")
+time.sleep(2)
+driver.quit()
