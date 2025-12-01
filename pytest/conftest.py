6ddd9459c1815207c91cdfcabfbaae10ee91fd98
Ashokcrestha <ashokstha51@gmail.com>
2025-12-01 07:49:41 +0545

    pytest

---
+++ pytest/conftest.py
@@ -0,0 +1,13 @@
+#conftest 
+import pytest
+from selenium import webdriver
+
+pytest.fixture
+
+def browser():
+    print("\nOpening Chrome browser...")
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+    print("\nClosing browser...")
+    driver.quit()
