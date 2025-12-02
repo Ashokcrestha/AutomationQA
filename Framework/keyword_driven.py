26866e8039bae5867ebf209de4e18d3777fdcc7c
Ashokcrestha <ashokstha51@gmail.com>
2025-12-02 12:09:34 +0545

    keyword F

---
+++ Framework/keyword_driven.py
@@ -0,0 +1,23 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+import csv, time
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+
+def perform_action(keyword, locator, value):
+    element = driver.find_element(By.ID, locator)
+
+    if keyword == "enterText":
+        element.send_keys(value)
+    elif keyword == "click":
+        element.click()
+
+with open("Framework/keywords.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        perform_action(row["keyword"], row["locator"], row["value"])
+                       
+
+time.sleep(2)
+driver.quit()
